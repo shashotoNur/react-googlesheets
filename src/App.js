dc7e37bf4419851c81ca35ff31e5079454f77192
Nurul Aziz <nurulaziz.nz@gmail.com>
2021-05-14 14:27:57 +0600

    end commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component
+{
+    constructor(props)
+    {
+        super(props)
+    
+        this.state =
+        {
+            name: '',
+            age: '',
+            salary: '',
+            hobby: ''
+        }
+    }
+
+    changeHandler = (event) =>
+    {
+        this.setState( {[event.target.name] : event.target.value} );
+    }
+
+    submitHandler = (event) =>
+    {
+        event.preventDefault();
+        console.log(this.state);
+
+        axios.post('{CONNECTION URL}', this.state)
+            .then((response) => { console.log(response); })
+    }
+    
+    render()
+    {
+        const { name, age, salary, hobby } = this.state;
+
+        return (
+
+        <Container fluid className="container">
+            <Header as='h2'>React Google Sheets!</Header>
+
+            <Form className="form" onSubmit={this.submitHandler}>
+            <Form.Field>
+                <label>Name</label>
+                <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+            </Form.Field>
+
+            <Form.Field>
+                <label>Age</label>
+                <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+            </Form.Field>
+
+            <Form.Field>
+                <label>Salary</label>
+                <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+            </Form.Field>
+
+            <Form.Field>
+                <label>Hobby</label>
+                <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+            </Form.Field>
+            
+            <Button color="blue" type='submit'>Submit</Button>
+            </Form>
+
+        </Container>
+
+        )
+    }
 }
-
-export default App;
